a2be95ee8dc0100ba27ed1161909e4dbe7863e98
Pavel <pavelnasennikk@gmail.com>
2022-10-14 16:08:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    int amountOfBricksNasennik = 55;
+    int containerCapacityNasennik = 8;
+    int pelnyNasennik = amountOfBricksNasennik/containerCapacityNasennik;
+    System.out.println("Number of full Boxes: " + pelnyNasennik);
+    int allNasennik = pelnyNasennik+1;
+    System.out.println("Amount of Boxes: " + allNasennik);
+    // Need to continue this program
+    int procentNasennik = ;
+
+    }
+}
